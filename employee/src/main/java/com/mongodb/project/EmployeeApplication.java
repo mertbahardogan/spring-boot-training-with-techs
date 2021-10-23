eb832860341c377f318c971889548814b00c82de
Mert Bahardogan <mert.bahardogan@icterra.com>
2021-10-23 23:44:52 +0300

    Swagger ui updated new version

--- employee/src/main/java/com/mongodb/project/EmployeeApplication.java
+++ employee/src/main/java/com/mongodb/project/EmployeeApplication.java
@@ -1,7 +1,6 @@
 package com.mongodb.project;
 
 import com.mongodb.project.employee.dataAccess.EmployeeDao;
-import jdk.javadoc.doclet.Doclet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -9,13 +8,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackageClasses = EmployeeDao.class)
-@EnableSwagger2WebMvc
+@Configuration
 public class EmployeeApplication {
     public static void main(String[] args) {
         SpringApplication.run(EmployeeApplication.class, args);
@@ -25,8 +27,21 @@ public class EmployeeApplication {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.mongodb.project"))
+                .apis( RequestHandlerSelectors.basePackage( "com.mongodb.project"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "Spring Boot Simple REST API",
+                "Spring Boot Simple REST API Swagger Documentation",
+                "Version 1",
+                "urn:tos",
+                new Contact("Admin", "www.mertbahardogan.com", "info@mertbahardogan.com"),
+                "Apache 2.0",
+                "https://www.apache.org/licenses/LICENSE-2.0",
+                new ArrayList<>());
     }
 }
