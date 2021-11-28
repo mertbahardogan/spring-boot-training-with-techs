c43588aed2cfd320793db70081a73de2fa8fc49e
Mert Bahardogan <mert.bahardogan@icterra.com>
2021-11-28 19:10:29 +0300

    Validation messages changed.

--- common/src/main/java/com/mongodb/project/common/dtos/EmployeeDTO.java
+++ common/src/main/java/com/mongodb/project/common/dtos/EmployeeDTO.java
@@ -1,23 +1,27 @@
 package com.mongodb.project.common.dtos;
 
 import lombok.Data;
+
 import javax.validation.constraints.*;
 
 @Data
 public class EmployeeDTO {
+    //TODO: move strings to another constant file.
 
-    @NotBlank(message = "Bu alan zorunludur.")
+    @NotBlank(message = "Required")
+    @Size(min = 2, max = 10, message = "Name field '${validatedValue}' must be between {min} and {max} characters long")
     private String name;
 
-    @NotBlank(message = "Bu alan zorunludur.")
+    @NotBlank(message = "Required")
     private String lastName;
 
-    @Min(value=1,message = "Salary cannot be lower than 1.")
+    @Min(value = 1, message = "'${validatedValue}' must be upper than {value}")
     private int salary;
 
-    @Min(value=18,message = "Age cannot be lower than 18.")
-    @Max(value=65,message = "Age cannot be higher than 65.")
+    @Min(value = 18, message = "'${validatedValue}' must be upper than {value}")
+    @Max(value = 65, message = "'${validatedValue}' must be lower than {value}")
     private int age;
 
+    @NotNull(message = "Required")
     private boolean isManager;
 }
